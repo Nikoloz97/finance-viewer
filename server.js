05db4a96f8041b3cb149aed348b83ca9ce5b0bd2
Nickolaus Gotsiridze <ngotsiridze@accumtech.com>
2024-04-20 12:34:58 -0400

    Created mongodb connection in server.js; created private file to store connection

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
+require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = 5000;
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
@@ -11,5 +38,5 @@ app.get("/", (req, res) => {
 });
 
 app.get("/countries", (req, res) => {
-  res.send("Armenia!");
+  res.send("Azerbaijan!");
 });
