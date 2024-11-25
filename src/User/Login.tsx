d3b99ea9dbf8cc7bf431e952208e02e9ff9317d6
Nickolaus Gotsiridze <ngotsiridze@accumtech.com>
2024-11-25 00:17:06 -0500

    Added shadcn form file; updated login page to use shadcn form

--- src/User/Login.tsx
+++ src/User/Login.tsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
-import { Button, Checkbox, Form, Header } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
 import { Link, useNavigate } from "react-router-dom";
 import { UseContextCheck } from "../CustomHooks/UseContextCheck";
 import ErrorPopup from "../CustomHooks/ErrorPopup/ErrorPopup";
 import "./User.css";
+import { Button } from "../ShadcnComponents/Button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ShadcnComponents/Form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Input } from "../ShadcnComponents/Input";
 
 const Login = () => {
   const { setUser } = UseContextCheck();
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [error, setError] = useState({
     isErrorFadingIn: false,
     isErrorFadingOut: false,
@@ -18,15 +30,36 @@ const Login = () => {
     message: "",
   });
 
-  const handleSubmit = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
+  const formSchema = z.object({
+    username: z
+      .string()
+      .min(5, {
+        message: "Username must be at least 5 characters",
+      })
+      .max(50),
+    password: z
+      .string()
+      .min(5, {
+        message: "Password must be at least 5 characters",
+      })
+      .max(50),
+  });
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     const response = await fetch("/user/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify(values),
     });
 
     const responseJson = await response.json();
@@ -60,24 +93,51 @@ const Login = () => {
   return (
     <div className="Login-Form">
       <Header textAlign="center">Welcome Back</Header>
-      <Form onSubmit={handleSubmit}>
-        <Form.Field>
-          <label>Username: </label>
-          <input onChange={(e) => setUsername(e.target.value)} />
-        </Form.Field>
-        <Form.Field>
-          <label>Password: </label>
-          <input onChange={(e) => setPassword(e.target.value)} />
-        </Form.Field>
-        <Form.Field className="Remember-Forgot-Container">
-          <Checkbox label="Remember me" />
-          <Link style={{ color: "white" }} to={"/user/forgot-password"}>
-            Forgot Password?
-          </Link>
-        </Form.Field>
-        <Form.Field className="Login-Button-Container">
-          <Button type="submit" content="Login" />
-        </Form.Field>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username:</FormLabel>
+                <FormControl>
+                  <Input {...field} className="text-black" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password:</FormLabel>
+                <FormControl>
+                  <Input {...field} className="text-black" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* <FormField>
+            <label>Password: </label>
+            <input onChange={(e) => setPassword(e.target.value)} />
+          </FormField> */}
+          {/* <FormField className="Remember-Forgot-Container">
+            <Checkbox label="Remember me" />
+            <Link style={{ color: "white" }} to={"/user/forgot-password"}>
+              Forgot Password?
+            </Link>
+          </FormField> */}
+
+          <div className="Login-Button-Container">
+            <Button className="dark" type="submit">
+              Login
+            </Button>
+          </div>
+        </form>
       </Form>
 
       <Link className="No-Account-Container" to={"/user/signup"}>
