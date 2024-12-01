6e6a6ceae4dec1af84ea6c522b81d0ed55066ff4
Nickolaus Gotsiridze <ngotsiridze@accumtech.com>
2024-12-01 16:13:13 -0500

    Investments: added add investment button; improved styling

--- src/Investments/Investments.tsx
+++ src/Investments/Investments.tsx
@@ -1,56 +1,80 @@
 import React from "react";
 import "./Investments.css";
+import { Plus } from "lucide-react";
+import { Button } from "../ShadcnComponents/Button";
 
 const Investments = () => {
-  const mockInvestments = [
+  const mockInvestmentReports = [
     {
-      // TODO: is investmentId and userId right way to do this??
-      investmentId: 1,
-      brokerage: "Webull",
+      investmentReportId: 1,
       userId: 1,
+      brokerageName: "Webull",
       balance: 7972.64,
       balanceDate: "10-31-2024",
-      // TODO: put these in investment details table?
-      prevBalanceDate: "09-30-2024",
-      prevBalanceDiff: 88.03,
-      // These are all relative to prev balance
-      depositAmount: 0,
-      withdrawalAmount: 0,
-      dividends: 2.07,
     },
     {
-      investmentId: 2,
-      brokerage: "Vanguard",
+      investmentReportId: 2,
+      brokerageName: "Vanguard",
       userId: 1,
       balance: 3118.64,
       balanceDate: "10-31-2024",
-      // TODO: put these in investment details table?
-      prevBalanceDate: "09-30-2024",
-      prevBalanceDiff: -14.01,
-      // These are all relative to prev balance
-      depositAmount: 0,
-      withdrawalAmount: 0,
-      dividends: 9.63,
     },
     {
-      investmentId: 3,
-      brokerage: "Fidelity",
+      investmentReportId: 3,
+      brokerageName: "Fidelity",
       userId: 1,
       balance: 2775.38,
       balanceDate: "09-30-2024",
-      // TODO: put these in investment details table?
       prevBalanceDate: "06-30-2024",
       prevBalanceDiff: 152.61,
-      // These are all relative to prev balance
       depositAmount: 0,
       withdrawalAmount: 0,
       dividends: null,
     },
   ];
+
+  const mockInvestmentReportDetails = [
+    {
+      investmentReportDetailId: 1,
+      investmentReportId: 1,
+      depositAmount: 0,
+      withdrawalAmount: 0,
+      dividends: 2.07,
+    },
+    {
+      investmentReportDetailId: 2,
+      investmentReportId: 2,
+      depositAmount: 0,
+      withdrawalAmount: 0,
+      dividends: 9.63,
+    },
+    {
+      investmentReportDetailId: 3,
+      investmentReportId: 3,
+      depositAmount: 0,
+      withdrawalAmount: 0,
+      dividends: null,
+    },
+  ];
+
+  const handleAddInvestmentClick = () => {
+    console.log("hello");
+  };
+
   return (
     <div className="Investments-Page">
       <div className="Investments-List-Container">
-        <div className="Investments-List-Rectangle">Investments List</div>
+        <div className="Investments-List-Rectangle">
+          <Button
+            style={{
+              height: "90%",
+              backgroundColor: "var(--secondary-background-color)",
+            }}
+            onClick={handleAddInvestmentClick}
+          >
+            <Plus />
+          </Button>
+        </div>
       </div>
       <div className="Investment-Display-Container">Investment Display</div>
     </div>
