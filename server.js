47141695600f4b80dfe52b6d84c62563e1d08f6c
Nickolaus Gotsiridze <nick.gotsy@gmail.com>
2025-01-26 13:42:25 -0500

    specified cors origin

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://financeviewer.azurewebsites.net", // Replace with your frontend URL
+  })
+);
+
 app.use(bodyParser.json());
 
 app.use("/user", userRouter);
