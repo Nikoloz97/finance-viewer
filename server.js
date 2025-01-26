a2cb75ef21132d56d2b33a42935b7c3324e1e54e
Nickolaus Gotsiridze <nick.gotsy@gmail.com>
2025-01-26 12:00:03 -0500

    Defined api routes before serving static files

--- server.js
+++ server.js
@@ -8,19 +8,19 @@ import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
-const port = process.env.PORT;
-
-// production script
-app.use(express.static("./build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
-});
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/user", userRouter);
 app.use("/investments", investmentsRouter);
+
+// production script ("serves static files")
+app.use(express.static("./build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
