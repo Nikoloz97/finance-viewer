ec65c59627f8e1739c5c5a8d8d502bfc66d925a3
Nickolaus Gotsiridze <nick.gotsy@gmail.com>
2025-01-25 22:47:44 -0500

    Created production script

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import userRouter from "./routes/UserRoutes.js";
 import investmentsRouter from "./routes/InvestmentsRoutes.js";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// production script
+app.use(express.static("./client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.use(cors());
 app.use(bodyParser.json());
