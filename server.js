14d782a76ff46b7c214437e78135600dbe59a8c4
Nickolaus Gotsiridze <nick.gotsy@gmail.com>
2025-01-26 11:40:26 -0500

    Removed port 5000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import investmentsRouter from "./routes/InvestmentsRoutes.js";
 import path from "path";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 // production script
 app.use(express.static("./build"));
