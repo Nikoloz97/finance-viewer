ea850f2fc7c1e3417d06e552e22ded07b128c474
NickGotsy <nick.gotsy@gmail.com>
2025-01-28 14:19:55 -0500

    Uploading artifacts is not just in build folder

--- server.js
+++ server.js
@@ -7,25 +7,20 @@ import path from "path";
 import dotenv from "dotenv";
 
 const app = express();
-dotenv.config();
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "https://financeviewer.azurewebsites.net",
-  })
-);
+app.use(cors());
 
 app.use(bodyParser.json());
 
 app.use("/user", userRouter);
 app.use("/investments", investmentsRouter);
 
-// production script ("serves static files")
-// app.use(express.static("./build"));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "build", "index.html"));
-// });
+//production script ("serves static files")
+app.use(express.static("./build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
