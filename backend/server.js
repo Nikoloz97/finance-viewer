9faab3298eacbe48dadc432806cd811db8fc5cb0
Nickolaus Gotsiridze <nick.gotsy@gmail.com>
2025-01-31 20:11:12 -0500

    Manually added Access-Control-Allow-Origin header to header

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,19 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//   })
+// );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(bodyParser.json());
 
