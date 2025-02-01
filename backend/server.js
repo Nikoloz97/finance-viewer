d27d4cd59d2d0c92e992af96183a23642b238d64
Nickolaus Gotsiridze <nick.gotsy@gmail.com>
2025-01-31 19:59:41 -0500

    Updated cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(bodyParser.json());
 
