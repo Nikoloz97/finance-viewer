8ae7823e0a0c05dfcf678ce87c9d4a0c392cba7c
Nikoloz97 <nick.gotsy@gmail.com>
2025-02-23 14:04:40 -0500

    Profile: created signout button with functionality

--- frontend/src/Profile.tsx
+++ frontend/src/Profile.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Button } from "semantic-ui-react";
 import { UseContextCheck } from "./CustomHooks/UseContextCheck";
 import { useNavigate } from "react-router-dom";
